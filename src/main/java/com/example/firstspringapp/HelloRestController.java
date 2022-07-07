8153482fb45b87353b7b7eda03e5fdf1585897ff
Yogesh More <iyogesh.more1998@gmail.com>
2022-07-08 02:57:45 +0530

    Added : Put Mapping is added.

--- src/main/java/com/example/firstspringapp/HelloRestController.java
+++ src/main/java/com/example/firstspringapp/HelloRestController.java
@@ -28,4 +28,9 @@ public class HelloRestController {
     {
         return "Hello " + user.getFirstName() + " "+  user.getLastName() + "!";
     }
+    @PutMapping("/put/{firstName}")
+    public String SayHelloPut(@PathVariable String firstName,@RequestParam(value = "lastName") String lastName)
+    {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
 }
